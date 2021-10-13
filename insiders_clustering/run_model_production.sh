fa1f8cc7f17f386dbcc103d9c7656d10f03fdd22
meigarom <meigarom.lopes@loft.com.br>
2021-10-13 15:34:42 -0300

    fix minor error

--- insiders_clustering/run_model_production.sh
+++ insiders_clustering/run_model_production.sh
@@ -3,6 +3,6 @@ data=$(date +'%Y-%m-%dT%H:%M:%S')
 
 #_ path 
 path='/home/ubuntu/pa005_insiders_clustering/insiders_clustering'
-path_to_envs='/home/ubuntu/.pyenv/versions/3.8.0/envs/pa005insidersclustering/bin'
+path_to_envs='/home/ubuntu/.local/bin/'
 
 $path_to_envs/papermill $path/src/models/c10-mdfl-deploy.ipynb $path/reports/c109-mdfl-deploy-$data.ipynb
