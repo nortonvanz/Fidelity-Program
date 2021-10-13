edd94ed2c8c5cb52ef7ec07bcd75ce2414a6e72f
meigarom <meigarom.lopes@loft.com.br>
2021-10-13 15:30:12 -0300

    update run model in production

--- insiders_clustering/run_model_production.sh
+++ insiders_clustering/run_model_production.sh
@@ -5,4 +5,4 @@ data=$(date +'%Y-%m-%dT%H:%M:%S')
 path='/home/ubuntu/pa005_insiders_clustering/insiders_clustering'
 path_to_envs='/home/ubuntu/.pyenv/versions/3.8.0/envs/pa005insidersclustering/bin'
 
-$path_to_envs/papermill $path/src/models/look_c0.9-mdfl-deploy.ipynb $path/reports/look_c0.9-mdfl-deploy.ipynb_$data.ipynb
+$path_to_envs/papermill $path/src/models/c10-mdfl-deploy.ipynb $path/reports/c109-mdfl-deploy-$data.ipynb
