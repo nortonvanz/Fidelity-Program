90accc0605cda458ed034d752e41604d0e8e18c7
nortonvanz <nortonmv@gmail.com>
2022-08-31 13:34:21 -0300

    bash file to run prod file with  papermill

--- run_model.sh
+++ run_model.sh
@@ -1,11 +1,13 @@
 # variable
 data=$(date +'%Y-%m-%dT%H:%M:%S')
 
-# path 
+
+# path (root of project) 
 path='/Users/home/repos/pa005_fidelity_program/'
 
-# execute papermill (find file: which papermill on project root folder)
+# path where papermill is installed on env (find: 'which papermill' on project root folder)
 path_to_envs='/Users/home/opt/anaconda3/envs/pa005_clustering/bin'
+
 
 $path_to_envs/papermill $path/src/models/9.0-nmv-deploy.ipynb $path/reports/9.0-nmv-deploy_$data.ipynb
 
